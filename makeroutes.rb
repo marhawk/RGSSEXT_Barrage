ab5cde82fa5ab72952998352aef76a92b72cdfab
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-26 21:28:49 +0800

    Update makeroutes.rb

--- makeroutes.rb
+++ makeroutes.rb
@@ -1,44 +1,16 @@
 $data_routes = {
-  "origin_2" => ["Graphics/Particles/0.png",
-  "0",
-  "(0.0001*(w-150)**3+337.5).to_i",
+  "origin" => ["Graphics/Particles/0.png",
+  "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
+  "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
   300
              ],
-  "origin_4" => ["Graphics/Particles/0.png",
-  "-(0.0001*(w-150)**3+337.5).to_i",
+  
+  "inverted" => ["Graphics/Particles/1.png",
   "0",
-  300
-             ],
-  "origin_6" => ["Graphics/Particles/0.png",
-  "(0.0001*(w-150)**3+337.5).to_i",
   "0",
   300
              ],
-  "origin_8" => ["Graphics/Particles/0.png",
-  "0",
-  "-(0.0001*(w-150)**3+337.5).to_i",
-  300
-             ],
-  "inverted_2" => ["Graphics/Particles/1.png",
-  "0",
-  "(0.0001*(w-150)**3+337.5).to_i",
-  300
-             ],
-  "inverted_4" => ["Graphics/Particles/1.png",
-  "-(0.0001*(w-150)**3+337.5).to_i",
-  "0",
-  300
-             ],
-  "inverted_6" => ["Graphics/Particles/1.png",
-  "(0.0001*(w-150)**3+337.5).to_i",
-  "0",
-  300
-             ],
-  "inverted_8" => ["Graphics/Particles/1.png",
-  "0",
-  "-(0.0001*(w-150)**3+337.5).to_i",
-  300
-             ],
+
 }
 HCL.makecache("Data/Routes.rxdata")
 HCL.loadcache("Data/Routes.rxdata")
