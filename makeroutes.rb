0d4ad487329affa511620c18ff147998627f6472
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-26 22:02:38 +0800

    Update makeroutes.rb

--- makeroutes.rb
+++ makeroutes.rb
@@ -2,13 +2,15 @@ $data_routes = {
   "origin" => ["Graphics/Particles/0.png",
   "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
   "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
-  300
+  300,
+  (0..360),
              ],
   
   "inverted" => ["Graphics/Particles/1.png",
   "0",
   "0",
-  300
+  300,
+  [0],
              ],
 
 }
