c287804f3b8132bdc3cefc944d55f79c5341c494
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-27 08:48:06 +0800

    Update makeroutes.rb

--- makeroutes.rb
+++ makeroutes.rb
@@ -1,28 +1,25 @@
 $data_routes = {
   "origin" => ["Graphics/Particles/0.png",
-  "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
-  "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
-  300,
+  "((0.001*(w-50)**3+125)*Math.cos(Math::PI*s/180)).to_i",
+  "((0.001*(w-50)**3+125)*Math.sin(Math::PI*s/180)).to_i",
+  150,
   (0..360).to_a,
-             ],
+               ],
   
   "inverted" => ["Graphics/Particles/1.png",
- "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
-  "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
-  300,
+ "((0.001*(w-50)**3+125)*Math.cos(Math::PI*s/180)).to_i",
+  "((0.001*(w-50)**3+125)*Math.sin(Math::PI*s/180)).to_i",
+  150,
   (0..360).to_a,
-             ],
+                 ],
 
 }
 $data_emitter = {
-  "origin" => [
-  [],
-             ],
-  
-  "inverted" => [
-  [],
-             ],
+  "circle" => "(0..360).each{|s|self.fire(%d,%d,route,s)}",
+  "circle_std" => "(0..45).each{|s|self.fire(%d,%d,route,s*8)}",
 
 }
 HCL.makecache("Data/Routes.rxdata")
 HCL.loadcache("Data/Routes.rxdata")
+HCL.makeemitter("Data/Emitter.rxdata")
+HCL.loademitter("Data/Emitter.rxdata")
