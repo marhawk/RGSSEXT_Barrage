31bd39f721b67ef31c2756c64b7b287c9a3d12a7
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-27 14:50:49 +0800

    4.3 Version -Release[4215]

--- makeroutes.rb
+++ makeroutes.rb
@@ -4,6 +4,7 @@ $data_routes = {
   "((0.001*(w-50)**3+125)*Math.sin(Math::PI*s/180)).to_i",
   150,
   (0..360).to_a,
+  ["",""],
                ],
   
   "inverted" => ["Graphics/Particles/1.png",
@@ -11,19 +12,23 @@ $data_routes = {
   "((0.001*(w-50)**3+125)*Math.sin(Math::PI*s/180)).to_i",
   150,
   (0..360).to_a,
+  ["",""],
                  ],
-
+  "clockwise" => ["Graphics/Particles/2.png",
+  "((0.001*(w-50)**3+125)*Math.cos(Math::PI*s2/180)).to_i",
+  "((0.001*(w-50)**3+125)*Math.sin(Math::PI*s2/180)).to_i",
+  150,
+  (0..360).to_a,
+  ["s2=s","s2=(s2+1)%360"],
+               ],
+  
 }
 $data_emitter = {
-  "circle" => "(0..360).each{|s|self.fire(%d,%d,route,s,standpoint)}",
-  "circle_std" => "(0..30).each{|s|self.fire(%d,%d,route,s*12,standpoint)}",
+  "circle" => "(0..360).each{|s|fire(%d,%d,route,s,standpoint)}",
+  "circle_std" => "(0..30).each{|s|fire(%d,%d,route,s*12,standpoint)}",
 
 }
-
-HCL.makecache("Data/Routes.rxdata")
-# Run makecache only when $data_routes is changed
-HCL.loadcache("Data/Routes.rxdata")
-
-HCL.makeemitter("Data/Emitter.rxdata")
-# Run makeemitter only when $data_emitter is changed
-HCL.loademitter("Data/Emitter.rxdata")
+$hcl.makecache("Data/Routes.rxdata")
+$hcl.loadcache("Data/Routes.rxdata")
+$hcl.makeemitter("Data/Emitter.rxdata")
+$hcl.loademitter("Data/Emitter.rxdata")
