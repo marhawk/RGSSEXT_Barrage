bca4d423510200772064777aa22e03444e16215b
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-27 08:08:04 +0800

    Update makeroutes.rb

--- makeroutes.rb
+++ makeroutes.rb
@@ -3,14 +3,24 @@ $data_routes = {
   "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
   "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
   300,
-  (0..360),
+  (0..360).to_a,
              ],
   
   "inverted" => ["Graphics/Particles/1.png",
  "((0.0001*(w-150)**3+337.5)*Math.cos(Math::PI*s/180)).to_i",
   "((0.0001*(w-150)**3+337.5)*Math.sin(Math::PI*s/180)).to_i",
   300,
-  (0..360),
+  (0..360).to_a,
+             ],
+
+}
+$data_emitter = {
+  "origin" => [
+  [],
+             ],
+  
+  "inverted" => [
+  [],
              ],
 
 }
