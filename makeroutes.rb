a34666fe1cf5869937f91bf3c30c75bc3089df96
Michelle Winderic <Michelle-Winderic@users.noreply.github.com>
2014-06-27 12:12:07 +0800

    4.0 Version -Release[14062711]
    This is the basic database of the system.

--- makeroutes.rb
+++ makeroutes.rb
@@ -19,7 +19,11 @@ $data_emitter = {
   "circle_std" => "(0..30).each{|s|self.fire(%d,%d,route,s*12,standpoint)}",
 
 }
+
 HCL.makecache("Data/Routes.rxdata")
+# Run makecache only when $data_routes is changed
 HCL.loadcache("Data/Routes.rxdata")
+
 HCL.makeemitter("Data/Emitter.rxdata")
+# Run makeemitter only when $data_emitter is changed
 HCL.loademitter("Data/Emitter.rxdata")
